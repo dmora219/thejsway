a58654e89b347fb8dee280f58b6a0de880da4050
dmora219 <dmora14633@gmail.com>
2023-11-13 10:26:27 -0600

    chapter 1 exercises

---
+++ chapter1/script.js
@@ -0,0 +1,18 @@
+let name = "David"
+let age = 33;
+console.log(`My name is ${name} i am ${age} years old`);
+console.log(6 / 3)
+console.log(4 + 3)
+console.log("4 + 5")
+console.log("4" + "5")
+
+
+
+
+
+
+
+
+
+
+
